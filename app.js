f782216e3c66567f257398d6641b8c63d0ad07a3
Davies <davis24n@gmail.com>
2022-11-21 08:23:09 +0300

    nft first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const buttonOpen = document.querySelector('[data-toggle ="open"]')
+const buttonClose = document.querySelector('[data-toggle ="close"]')
+const overlayEl = document.querySelector('.nav_overlay')
+const menuEl = document.querySelector('.nav_menu')
+
+const toggleMenu = () => {
+    menuEl.classList.toggle('active')
+    overlayEl.classList.toggle('active')
+    document.body.classList.toggle('scroll_false')
+}
+
+buttonOpen.addEventListener('click', toggleMenu)
+buttonClose.addEventListener('click', toggleMenu)
+overlayEl.addEventListener('click', toggleMenu)
+window.addEventListener('resize', () => {
+    if(this.innerWidth >= 1024) if(menuEl.classList.contains('active')) toggleMenu()
+})
+
+// 64 em = 1024 // 48 em = 768px 
+
+const sr = ScrollReveal({
+    distance: '25px',
+    duration: 1500,
+    easing: 'cubic-bezier(.68, -.55, .265, 1.55)'
+})
+
+sr.reveal('.showcase_text_group',{
+        origin: 'top'
+    }
+)
+
+sr.reveal('.showcase_image_group',{
+      origin: 'bottom'
+    }
+)
